058ff8dc00ebc1563642e1415883492011527a8c
Bulat <bulat0talipov@gmail.com>
2023-08-03 11:40:07 +0300

    update 03.08.2023

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,8 @@ import file from './modules/file';
 import projectContent from './modules/project-content';
 import landing from './modules/landing';
 import fancyboxFunc from './modules/fancybox-func';
+import initSelects from './modules/initSelect';
+// import customSelectControl from './modules/customSelectControl';
 
 documenReady(() => {
   window.complex_api = {};
@@ -35,4 +37,6 @@ documenReady(() => {
   projectContent();
   landing();
   fancyboxFunc();
+  initSelects();
+  // customSelectControl();
 });
